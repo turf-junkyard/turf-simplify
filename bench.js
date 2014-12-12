c238b64db67726ec1e4fc09c0a2f272e8d901568
morganherlocker <morgan.herlocker@gmail.com>
2014-12-12 14:52:37 -0500

    added benching

--- bench.js
+++ bench.js
@@ -3,15 +3,19 @@ var Benchmark = require('benchmark');
 var fs = require('fs');
 
 //console.log(JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/complexLine.geojson')))
-var line = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/complexLine.geojson'));
-var poly = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/complexPolygon.geojson'));
+var line = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/linestring.geojson'));
+var poly = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/polygon.geojson'));
+var argentina = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/argentina.geojson'));
 var suite = new Benchmark.Suite('turf-simplify');
 suite
   .add('turf-simplify#LineString',function () {
-    simplify(line, 50, 0);
+    simplify(line, .1, 0);
   })
   .add('turf-simplify#Polygon',function () {
-    simplify(poly.features, 50, 0);
+    simplify(poly, .01, 0);
+  })
+  .add('turf-simplify#Argentina',function () {
+    simplify(argentina, .05, 0);
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
