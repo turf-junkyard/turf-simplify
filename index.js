1cbb711ab649ed9f12f7646fe2eb4c7687e19cc6
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-20 17:39:43 -0800

    Add example

--- index.js
+++ index.js
@@ -12,9 +12,35 @@ var simplify = require('simplify-js');
  * a higher-quality simplification with a different algorithm
  * @return {Feature} a simplified feature
  * @example
- * var quantization = 50;
- * var minimumArea = 0;
- * var simplified = turf.simplify(polys, quantization, minimumArea);
+ * var feature = turf.polygon([[
+ *  [-70.603637, -33.399918],
+ *  [-70.614624, -33.395332],
+ *  [-70.639343, -33.392466],
+ *  [-70.659942, -33.394759],
+ *  [-70.683975, -33.404504],
+ *  [-70.697021, -33.419406],
+ *  [-70.701141, -33.434306],
+ *  [-70.700454, -33.446339],
+ *  [-70.694274, -33.458369],
+ *  [-70.682601, -33.465816],
+ *  [-70.668869, -33.472117],
+ *  [-70.646209, -33.473835],
+ *  [-70.624923, -33.472117],
+ *  [-70.609817, -33.468107],
+ *  [-70.595397, -33.458369],
+ *  [-70.587158, -33.442901],
+ *  [-70.587158, -33.426283],
+ *  [-70.590591, -33.414248],
+ *  [-70.594711, -33.406224],
+ *  [-70.603637, -33.399918]
+ * ]]);
+ * var tolerance = 0.01;
+ * var simplified = turf.simplify(
+ *  feature, tolerance, false);
+ *
+ * //=feature
+ *
+ * //=simplified
  */
 module.exports = function(feature, tolerance, highQuality){
   if(feature.geometry.type === 'LineString') {
